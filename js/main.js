5de110c9d1ccb0bb6c052a81ffe9fcfa39a9c187
Lesiv Lyubomir <lyubas84@mail.ru>
2020-03-19 14:15:24 +0100

    feat:  rewrite modal in  jQuery

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
-document.addEventListener("DOMContentLoaded",function
+/*document.addEventListener("DOMContentLoaded",function
 (event){
     const modal= document.querySelector(".modal");
     const modalBtn= document.querySelectorAll("[data-toggle=modal]");
     const closeBtn= document.querySelector(".modal__close");
+
     const switchModal = () => {
         modal.classList.toggle("modal--visible");
     }
-   
+
     modalBtn.forEach(element=>{
         element.addEventListener("click", switchModal);
     });
+
     closeBtn.addEventListener("click", switchModal);
+});*/
+$(document).ready(function(){
+    var modal=$(".modal"),
+        modalBtn=$("[data-toggle=modal]"),
+        closeBtn=$(".modal__close");
+
+    modalBtn.on("click" ,function(){
+        modal.addClass("modal--visible");
+    });
+    closeBtn.on("click",function(){
+        modal.removeClass("modal--visible");
+    });
+    modal.on("click",function(){
+        modal.removeClass("modal--visible");
+    });
+
 
 });
