583af836cd3e5aec29085fe6dcd38291220f62f8
Lesiv Lyubomir <lyubas84@mail.ru>
2020-03-20 21:10:00 +0100

    feat:  add sliderr section

--- js/main.js
+++ js/main.js
@@ -31,7 +31,25 @@ $(document).ready(function(){
     });
     scrollBtn.on("click",function(){
         $("html, body").animate({ scrollTop: "0" });
-    })
+    });
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    });
+    var next=$(".swiper-button-next");
+    var prev=$(".swiper-button-prev");
+    var bullets=$(".swiper-pagination");
+
+    next.css("left", prev.width()+10+bullets.width()+10)
+    bullets.css("left", prev.width()+10)
 
 
 });
