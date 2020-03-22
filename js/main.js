69057848a039cd9f850c2e77ceb83b8d498f3b8f
Lesiv Lyubomir <lyubas84@mail.ru>
2020-03-22 15:05:57 +0100

    add footer

--- js/main.js
+++ js/main.js
@@ -14,26 +14,32 @@
 
     closeBtn.addEventListener("click", switchModal);
 });*/
-$(document).ready(function(){
-    var modal=$(".modal"),
-        modalBtn=$("[data-toggle=modal]"),
-        closeBtn=$(".modal__close"),
+$(document).ready(function () {
+    var modal = $(".modal"),
+        modalBtn = $("[data-toggle=modal]"),
+        closeBtn = $(".modal__close"),
         scrollBtn = $(".scrollBtn");
 
-    modalBtn.on("click" ,function(){
+    modalBtn.on("click", function () {
         modal.addClass("modal--visible");
     });
-    closeBtn.on("click",function(){
+    closeBtn.on("click", function () {
         modal.removeClass("modal--visible");
     });
-    modal.on("click",function(){
-        modal.removeClass("modal--visible");
-    });
-    scrollBtn.on("click",function(){
+    $(window).click(function(event){
+        console.log(event);
+        if(event.target.id == "myModal"){
+            modal.removeClass("modal--visible");
+        }
+    })
+    // modal.on("click", function () {
+    //     modal.removeClass("modal--visible");
+    // });
+    scrollBtn.on("click", function () {
         $("html, body").animate({ scrollTop: "0" });
     });
 
-    var mySwiper = new Swiper ('.swiper-container', {
+    var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         pagination: {
             el: '.swiper-pagination',
@@ -42,16 +48,42 @@ $(document).ready(function(){
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
-          },
+        },
     });
-    var next=$(".swiper-button-next");
-    var prev=$(".swiper-button-prev");
-    var bullets=$(".swiper-pagination");
+    var next = $(".swiper-button-next");
+    var prev = $(".swiper-button-prev");
+    var bullets = $(".swiper-pagination");
 
-    next.css("left", prev.width()+10+bullets.width()+10)
-    bullets.css("left", prev.width()+10)
+    next.css("left", prev.width() + 10 + bullets.width() + 10)
+    bullets.css("left", prev.width() + 10)
 
     new WOW().init();
-
-
+    //Validation form
+    $(".modal__form").validate({
+        errorClass:"invalid",
+        rules: {
+            // simple rule, converted to {required:true}
+            name: {
+                required: true,
+                minlength: 2
+            },
+            // compound rule
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: 
+            {   required: "Имя обязательно",
+                minlength: "имя не короче двух букв"
+            },
+            email: {
+                required: "обязательно укажите email",
+                email: "введите в формате name@domain.com"
+            }
+        }
+    });
+    //mask for phone
+    $('[type=tel]').mask('+7(000) 00-00-000',{placeholder: "+7(___)__-__-___"});
 });
